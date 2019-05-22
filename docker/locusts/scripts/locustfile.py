d2fbaf2aa5675b6476d490444dcb09bea05508e4
Mario Hercules <mariohercules@hotmail.com>
2019-05-22 10:09:16 -0300

    Update locust Dockerfile

--- docker/locusts/scripts/locustfile.py
+++ docker/locusts/scripts/locustfile.py
@@ -11,13 +11,19 @@ class SimpleTrafficRequest(TaskSet):
     
     @task(2)
     def search_for_blog_post(self):
-        self.client.get("/?s=ola")
+        self.client.get("/?s=ola-mundo")
     
-    @task(2)
+    @task(3)
     def search_for_blog_unpost(self):
         self.client.get("/?s=test")
 
+    @task(4)
+    def search_for_blog_unpost(self):
+        self.client.get("/locust/add-post.php")        
+
 class WebsiteUser(HttpLocust):         
-    host = os.getenv('TARGET_URL', "http://172.19.0.1")
+    host = os.getenv('TARGET_URL', "http://nginx")
     task_set = SimpleTrafficRequest                            
     min_wait = 5000
+    max_wait = 15000
+    #stop_timeout = 100
